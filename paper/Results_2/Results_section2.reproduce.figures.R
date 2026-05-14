cbeed3d7705e42e6c0cb7824aefc2cc71cc1cf5e
xueying66 <1370649586@qq.com>
2026-05-14 23:07:04 +0800

    Update Results_section2.reproduce.figures.R

--- paper/Results_2/Results_section2.reproduce.figures.R
+++ paper/Results_2/Results_section2.reproduce.figures.R
@@ -10,15 +10,15 @@ ggplot(mutation.burden.df, aes(x = rank, y = mutation.burden, color = type2))+
                   nudge_y = 0.1)+
   xlab('Rank') + ylab('Mutation burden')+
   geom_hline(yintercept = 9, color = "black")+
-  scale_color_manual(values = type_colors) +  # 这里指定颜色
+  scale_color_manual(values = type_colors) +  
   theme_bw()+
   theme(
     
     plot.title = element_text(hjust = 0.5, size = 25),
-    axis.title = element_text(size = 25, hjust = 0.5, color = 'black'),     # 坐标轴标题加粗
-    axis.text = element_text(size = 25, color = 'black'),      # 坐标轴刻度加粗
-    panel.border = element_rect(color = "black", linewidth = 1.5, fill = NA), # 加粗边框
-    axis.ticks.length = unit(0.4, "cm"),         # 增加刻度线长度
+    axis.title = element_text(size = 25, hjust = 0.5, color = 'black'),     
+    axis.text = element_text(size = 25, color = 'black'),    
+    panel.border = element_rect(color = "black", linewidth = 1.5, fill = NA),
+    axis.ticks.length = unit(0.4, "cm"),         
     
   )
 ggsave('/home/liuxueying/Proj_MetPCCelline/output/plot/three.dataset.mutation.burden.pdf', width = 12,height = 10)
@@ -35,15 +35,15 @@ load(file = '/home/liuxueying/Proj_MetPCCelline/output/MSI.Rdata')
 p1 <- oncoPrint(
   mutation.profile,
   get_type = function(x) {ifelse(x == 1, "MUT", "background")},
-  row_names_side = "left",          # 行名放置在左侧
-  show_heatmap_legend = F,      # 隐藏图例
-  alter_fun = alter_fun,            # 突变样式函数
-  col = col,                        # 颜色定义
-  show_column_names = FALSE,        # 不显示列名
-  show_row_names = T, # 不显示行名
-  top_annotation = col.annotation,  # 顶部注释
-  right_annotation = NULL,          # 确保右侧无条形图
-  show_pct = FALSE,                 # 禁用百分比显示
+  row_names_side = "left",          
+  show_heatmap_legend = F,      
+  alter_fun = alter_fun,            
+  col = col,                        
+  show_column_names = FALSE,       
+  show_row_names = T, 
+  top_annotation = col.annotation, 
+  right_annotation = NULL,         
+  show_pct = FALSE,                 
   row_names_gp = gpar(fontsize = 40, fontface = 'bold'),
   column_order = sample_order,
   row_order = gene.order
@@ -58,19 +58,19 @@ p2 <- Heatmap(mutation.frequency,
               col=colorRamp2(c(0,1),c('blue','red')),
               row_names_gp = gpar(fontsize = 40,fontface='bold'),
               width = unit(4, "cm"),
-              show_heatmap_legend = T,##显示图例
+              show_heatmap_legend = T,
               top_annotation=col.ha,
               show_column_names = F,
               cluster_columns = F,
               cluster_rows = F,
-              column_order = c("MSI.mutation.fequency", "MSI.no.mutation.fequency"), # 强制指定列顺序
+              column_order = c("MSI.mutation.fequency", "MSI.no.mutation.fequency"), 
               row_order = gene.order,
               name = 'Mutation \nfrequency',
               heatmap_legend_param = list(
-                title_gp = gpar(fontsize = 20, fontface = "bold"), # 图例标题字体
-                labels_gp = gpar(fontsize = 20),                  # 图例标签字体
-                legend_height = unit(10, "cm"),                    # 图例高度
-                legend_width = unit(5, "cm")                      # 图例宽度
+                title_gp = gpar(fontsize = 20, fontface = "bold"), 
+                labels_gp = gpar(fontsize = 20),                 
+                legend_height = unit(10, "cm"),                   
+                legend_width = unit(5, "cm")                     
               ))
 
 
@@ -111,10 +111,10 @@ ggplot(de.res, aes(x = log2FoldChange, y = -log10(padj),color=sig))+
   theme(
     legend.position = 'none',
     plot.title = element_text(hjust = 0.5, size = 30),
-    axis.title = element_text(size = 30, hjust = 0.5, color = 'black'),     # 坐标轴标题加粗
-    axis.text = element_text(size = 30, color = 'black'),      # 坐标轴刻度加粗
-    panel.border = element_rect(color = "black", linewidth = 1.5, fill = NA), # 加粗边框
-    axis.ticks.length = unit(0.4, "cm"),         # 增加刻度线长度
+    axis.title = element_text(size = 30, hjust = 0.5, color = 'black'),     
+    axis.text = element_text(size = 30, color = 'black'),      
+    panel.border = element_rect(color = "black", linewidth = 1.5, fill = NA), 
+    axis.ticks.length = unit(0.4, "cm"),
     
   )
 ggsave('/home/liuxueying/Proj_MetPCCelline/output/plot/de.hyper.non.volcano.pdf', width = 10, height = 10)
